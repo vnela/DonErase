f846c08a93419efe06ae32352e1cd4633e7890e3
unknown <venla.vihersaari@gmail.com>
2019-10-29 08:36:14 +0200

    ekat lisäykset: pomin korjaus ja gitignoren päivitys

---
+++ src/main/java/ohjelmistoprojekti/kyselysovellus/KyselysovellusApplication.java
@@ -0,0 +1,13 @@
+package ohjelmistoprojekti.kyselysovellus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyselysovellusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyselysovellusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ohjelmistoprojekti/kyselysovellus/KyselysovellusApplicationTests.java
@@ -0,0 +1,13 @@
+package ohjelmistoprojekti.kyselysovellus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyselysovellusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
