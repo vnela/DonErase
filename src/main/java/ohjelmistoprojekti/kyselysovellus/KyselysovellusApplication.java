5f635bb9038f2f03a6b4d8632d438a9a2db725bc
unknown <venla.vihersaari@gmail.com>
2019-11-05 15:28:30 +0200

    oikeat kysymykset lisätty commandlinerunneriin

--- src/main/java/ohjelmistoprojekti/kyselysovellus/KyselysovellusApplication.java
+++ src/main/java/ohjelmistoprojekti/kyselysovellus/KyselysovellusApplication.java
@@ -16,18 +16,22 @@ public class KyselysovellusApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(KyselysovellusApplication.class, args);
 	}
-	
+
 	@Bean
-	public CommandLineRunner questionDemo(QuestionRepository qRepository) { 
+	public CommandLineRunner questionDemo(QuestionRepository qRepository) {
 		return (args) -> {
 			log.info("Save some questions");
-		
-			qRepository.save(new Question("Mitä kuuluu?",""));
-	log.info("fetch all questions");
-			for (Question question: qRepository.findAll()) {
-				log.info(question.toString());
-}
 
-};
+			qRepository.save(new Question("Minkä vuoden opiskelija olet?", ""));
+			qRepository.save(new Question("Mihin profiiliin olet suuntautumassa/suuntautunut?", ""));
+			qRepository.save(new Question("Miten mielestäsi HOPS:iin perehdytettiin opintojen alussa?", ""));
+			qRepository.save(new Question("Minkälaisia ongelmia olet kohdannut opintojen edetessä HOPS:iin liittyen?", ""));
+			qRepository.save(new Question("Minkälaisia ominaisuuksia HOPS:in luomiseen voitaisiin lisätä?", ""));
+			log.info("fetch all questions");
+			for (Question question : qRepository.findAll()) {
+				log.info(question.toString());
+			}
+
+		};
 	}
 }
